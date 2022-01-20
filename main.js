f290fb5a241c01df5f8a14a82737d117dc96b049
paolabasso <91506356+paolabasso@users.noreply.github.com>
2022-01-20 20:59:07 -0300

    ScrollReveal Arrumado

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const scrollReveal = ScrollReveal({
-  origin: 'top',
+  origin: '#home',
   distance: '50px',
   duration: 500,
   reset: true
@@ -7,10 +7,29 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
   `
-  #home,
-  #crocheteiras, .cards, .card,
-  #tutoriais, .videos, .video,
+ 
+  #crocheteiras .cards, .card,
+  #tutoriais .videos, .video,
   footer
   `,
   { interval: 50 }
 )
+
+//Quando o mouse estiver dentro do card, o card precisa ficar cinza claro
+
+var cartoes = document.getElementsByClassName('card')
+
+console.log(cartoes)
+
+function entrar() {
+  alert('Deu certo!')
+}
+//cartoes.addEventListener('mouseenter', entrar)
+
+//entrar()
+
+console.log(cartoes[0])
+
+for (let i = 0; i < cartoes.length; i++) {
+  cartoes[i].addEventListener('mouseenter', entrar)
+}
